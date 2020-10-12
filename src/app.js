805f66ca3141721b2d221a1357fda40635c8da9b
mischkiey <mischkieycf@gmail.com>
2020-10-12 02:15:52 -0400

    Setup prisma client and database

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
 
+const { PrismaClient } = require('@prisma/client');
 
 const morgan = require('morgan');
 const cors = require('cors');
@@ -12,6 +13,7 @@ const { NODE_ENV } = require('./config');
 const e = require('express');
 
 const app = express();
+const prisma = new PrismaClient();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -21,9 +23,16 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-// Add token type
-// User service object for validation
-// Connect to PostgreSQL with Prisma
+const getUserByUsername = async(username) => {
+  const user = await prisma.user.findOne({where: { username }});
+  return user;
+}
+
+const insertNewUser = async(input) => {
+  await prisma.user.create({
+    data: input
+  });
+}
 
 const schema = buildSchema(`
   type Mutation {
@@ -44,42 +53,57 @@ const schema = buildSchema(`
   
 `);
 
-const fakeUserDatabase = {
-  username: 'admin',
-  password: 'pass',
-};
-
 const root = {
-  hello: () => {
-    return 'Hello, world!';
-  },
-
-  authUserLogInInput: ({ username, password }) => {
+  authUserLogInInput: async ({ username, password }) => {
     // Validate no username
     // Validate no password
-    if(username !== fakeUserDatabase.username) {
-      return 'Incorrect Username'
-    } 
+    // if(username !== fakeUserDatabase.username) {
+    //   return 'Incorrect Username'
+    // } 
     
-    if(password !== fakeUserDatabase.password) {
-      return 'Incorrect password'
+    // if(password !== fakeUserDatabase.password) {
+    //   return 'Incorrect password'
+    // }
+
+    try {
+      const user = await getUserByUsername(username);
+      console.log(user);
+    } catch(e) {
+      console.log(e);
+    } finally {
+      await prisma.$disconnect();
     }
 
     // Return token
     return 'Successfully logged in'
   },
 
-  postUserSignUpInput: ({ input }) => {
+  postUserSignUpInput: async ({ input }) => {
     // Validate fields
     console.log(input);
+    try {
+      await insertNewUser(input);
+      const user = await getUserByUsername(input.username);
+      console.log(user);
+    } catch(e) {
+      console.log(e);
+    } finally {
+      await prisma.$disconnect();
+    }
+
     return 'Successfully signed up'
   }
 }
 
-graphql(schema, 'mutation { postUserSignUpInput(input: {firstName: "M", lastName: "F", email: "E", username: "U", password: "P"}) }', root)
+graphql(schema, 'query { authUserLogInInput(username: "U", password: "P") }', root)
   .then((response) => {
     console.log(response);
   });
+
+// graphql(schema, 'mutation { postUserSignUpInput(input: {firstName: "M", lastName: "F", email: "E", username: "U", password: "P"}) }', root)
+//   .then((response) => {
+//     console.log(response);
+//   });
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
@@ -89,12 +113,14 @@ app.use('/graphql', graphqlHTTP({
 
 app.use(function errorHandler(error, req, res, next) { /* eslint-disable-line no-unused-var */
   let response;
+
   if (NODE_ENV === 'production') {
     response = { error: { message: 'Internal server error' } };
   } else {
     console.log(error);
     response = { message: error.message, error }
   };
+
   res.status(500).json(response);
 });
 
