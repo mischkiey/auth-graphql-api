79f6cadbb8419d42bebffc0ac6bea62f40afdf1b
mischkiey <mischkieycf@gmail.com>
2020-10-12 14:39:56 -0400

    Add username and password validation on postUserSignUpInput resolver fn

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const { NODE_ENV } = require('./config');
-const e = require('express');
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const prisma = new PrismaClient();
@@ -23,16 +25,10 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-const getUserByUsername = async(username) => {
-  const user = await prisma.user.findOne({where: { username }});
-  return user;
-}
-
-const insertNewUser = async(input) => {
-  await prisma.user.create({
-    data: input
-  });
-}
+// Todo:
+// Rename endpoints/fields
+// Factor out variables
+// Consider using fragments for practice
 
 const schema = buildSchema(`
   type Mutation {
@@ -53,18 +49,40 @@ const schema = buildSchema(`
   
 `);
 
+const getUserByUsername = async(username) => {
+  const user = await prisma.user.findOne({where: { username }});
+  return user;
+}
+
+const insertNewUser = async(input) => {
+  await prisma.user.create({
+    data: input
+  });
+}
+
+const validatePassword = (password) => {
+  if (password.length <= 8)
+    return 'Password must be longer than 8 characters';
+
+  if (password.length >= 72)
+    return 'Password must be shorter than 72 characters';
+
+  if (password.startsWith(' ') || password.endsWith(' '))
+    return 'Password must must not start or end with spaces';
+
+  if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password))
+    return 'Password must contain 1 upper case, 1 lower case, 1 number, and 1 special character';
+  
+  return null;
+}
+
+const hashPassword = (password) => {
+
+}
+
 const root = {
   authUserLogInInput: async ({ username, password }) => {
-    // Validate no username
-    // Validate no password
-    // if(username !== fakeUserDatabase.username) {
-    //   return 'Incorrect Username'
-    // } 
-    
-    // if(password !== fakeUserDatabase.password) {
-    //   return 'Incorrect password'
-    // }
-
+    // Validate if password is correct dummy
     try {
       const user = await getUserByUsername(username);
       console.log(user);
@@ -73,34 +91,36 @@ const root = {
     } finally {
       await prisma.$disconnect();
     }
-
-    // Return token
-    return 'Successfully logged in'
+    // Return jwt
+    return 'Successfully logged in';
   },
 
   postUserSignUpInput: async ({ input }) => {
-    // Validate fields
-    console.log(input);
+    const { username, password } = input;
     try {
+      const user = await getUserByUsername(username);
+      if(user) return 'Username already exists';
+
+      const passwordError = validatePassword(password);
+      if(passwordError) return passwordError;
+
       await insertNewUser(input);
-      const user = await getUserByUsername(input.username);
-      console.log(user);
+      return 'Successfully signed up';
     } catch(e) {
       console.log(e);
     } finally {
+      console.log('Hi')
       await prisma.$disconnect();
     }
-
-    return 'Successfully signed up'
   }
 }
 
-graphql(schema, 'query { authUserLogInInput(username: "U", password: "P") }', root)
+graphql(schema, 'query { authUserLogInInput(username: "O", password: "P") }', root)
   .then((response) => {
     console.log(response);
   });
 
-// graphql(schema, 'mutation { postUserSignUpInput(input: {firstName: "M", lastName: "F", email: "E", username: "U", password: "P"}) }', root)
+// graphql(schema, 'mutation { postUserSignUpInput(input: {firstName: "M", lastName: "F", email: "E", username: "O", password: "!Expelliarmus01"}) }', root)
 //   .then((response) => {
 //     console.log(response);
 //   });
